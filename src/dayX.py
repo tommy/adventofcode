a1a48bd4ed31072d02954fe93c6094219dda3f0f
Tommy R <tommy@users.noreply.github.com>
2021-12-05 08:39:58 -0800

    add template

---
+++ src/dayX.py
@@ -0,0 +1,17 @@
+def readlines():
+    with open("./input/dayX.txt") as f:
+        for line in f.readlines():
+            yield line.strip()
+
+
+def solve_one():
+    pass
+
+
+def solve_two():
+    pass
+
+
+if __name__ == "__main__":
+    print(solve_one())
+    print(solve_two())
