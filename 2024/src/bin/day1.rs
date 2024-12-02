2fe3565ea98320fa89a3b1f558ee1a3c9d6d7331
Tommy Rogers <tommy@stedi.com>
2024-12-02 12:55:22 -0800

    second star

--- 2024/src/bin/day1.rs
+++ 2024/src/bin/day1.rs
@@ -1,12 +1,37 @@
 use std::fs;
 
+use itertools::Itertools as _;
 use regex::Regex;
 
 fn main() {
-    println!("{}", solve_a("inputs/day1a.txt"));
+    println!("A: {}", solve_a("inputs/day1.txt"));
+    println!("B: {}", solve_b("inputs/day1.txt"));
 }
 
 fn solve_a(filename: &str) -> u32 {
+    let (mut lefts, mut rights) = parse(filename);
+
+    lefts.sort();
+    rights.sort();
+
+    lefts
+        .iter()
+        .zip(rights.iter())
+        .map(|(left, right)| left.abs_diff(*right))
+        .sum()
+}
+
+fn solve_b(filename: &str) -> i32 {
+    let (lefts, rights) = parse(filename);
+
+    let counts = rights.iter().counts();
+    lefts
+        .into_iter()
+        .map(|left| left * counts.get(&left).copied().unwrap_or_default() as i32)
+        .sum()
+}
+
+fn parse(filename: &str) -> (Vec<i32>, Vec<i32>) {
     let input = fs::read_to_string(filename).expect("Failed to read file");
     let pattern = Regex::new(r"(\d+)\s+(\d+)").unwrap();
     let mut lefts = Vec::new();
@@ -17,12 +42,5 @@ fn solve_a(filename: &str) -> u32 {
         rights.push(groups.get(2).unwrap().as_str().parse::<i32>().unwrap());
     }
 
-    lefts.sort();
-    rights.sort();
-
-    lefts
-        .iter()
-        .zip(rights.iter())
-        .map(|(left, right)| left.abs_diff(*right))
-        .sum()
+    (lefts, rights)
 }
